ce5786fb2e53884e3df3cd75e9a93ea99cbe9614
Seo JeongSeop <seo92js@gmail.com>
2023-05-15 01:22:02 +0900

    Initial commit

---
+++ src/main/java/side/collectionrecord/CollectionRecordApplication.java
@@ -0,0 +1,13 @@
+package side.collectionrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/side/collectionrecord/CollectionRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package side.collectionrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
