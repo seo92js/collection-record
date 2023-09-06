40bc5c97232bb378728e697138e068e4e6dd36ab
seojeongseop <seo92js@gmail.com>
2023-09-07 01:27:36 +0900

    websocket 에러 해결 중

--- src/main/resources/static/js/receive-notification.js
+++ src/main/resources/static/js/receive-notification.js
@@ -1,6 +1,5 @@
 //const notificationSocket = new WebSocket('ws://localhost:8080/notification');
-//const notificationSocket = new WebSocket('ws://ec2-52-79-198-114.ap-northeast-2.compute.amazonaws.com/notification');
-const notificationSocket = new WebSocket('ws://127.0.0.1:8080/notification');
+const notificationSocket = new WebSocket('ws://ec2-52-79-198-114.ap-northeast-2.compute.amazonaws.com/notification');
 
 notificationSocket.onopen = function() {
       const username = document.getElementById('loginUsername').value;
