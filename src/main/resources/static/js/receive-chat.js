f68398466c54e39ea021cbf44c1990ccfc0a7be1
seojeongseop <seo92js@gmail.com>
2023-09-06 23:09:14 +0900

    websocket 에러 해결 중

--- src/main/resources/static/js/receive-chat.js
+++ src/main/resources/static/js/receive-chat.js
@@ -1,6 +1,5 @@
 //const chatSocket = new WebSocket('ws://localhost:8080/chatroom');
-const currentPort = window.location.port;
-const chatSocket = new WebSocket('ws://ec2-52-79-198-114.ap-northeast-2.compute.amazonaws.com:' + currentPort + '/chatroom');
+const chatSocket = new WebSocket('ws://ec2-52-79-198-114.ap-northeast-2.compute.amazonaws.com/chatroom');
 
 chatSocket.onopen = function() {
       const username = document.getElementById('loginUsername').value;
