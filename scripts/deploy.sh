a8e8e938ad968eefe0bca5a5bcf9082f02bcbba7
Seo JeongSeop <seo92js@gmail.com>
2023-09-06 10:24:46 +0900

    deploy.sh 수정, 프로세스 잡는 부분에서 jar -> java

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl collection-record | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl collection-record | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션pid: $CURRENT_PID"
 
