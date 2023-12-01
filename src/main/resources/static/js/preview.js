7ea067e8bdb49eb32d55af1bb5e3b584394e7bbc
seo92js <seo92js@gmail.com>
2023-12-01 18:44:57 +0900

    bootstrap 적용 중

--- src/main/resources/static/js/preview.js
+++ src/main/resources/static/js/preview.js
@@ -1,3 +1,20 @@
+//function preview(input){
+//  $('#preview').empty();
+//
+//  if (input.files) {
+//    for (var i = 0; i < input.files.length; i++){
+//        var reader = new FileReader();
+//        reader.onload = function(e) {
+//            $('#preview').attr("src", e.target.result).addClass('posts-add__preview');
+//        };
+//
+//        reader.readAsDataURL(input.files[i]);
+//    }
+//  } else {
+//    $('#preview').empty();
+//  }
+//}
+
 function preview(input){
   $('#preview').empty();
 
@@ -5,7 +22,7 @@ function preview(input){
     for (var i = 0; i < input.files.length; i++){
         var reader = new FileReader();
         reader.onload = function(e) {
-            $('#preview').attr("src", e.target.result).addClass('posts-add__preview');
+            $('#preview').attr("src", e.target.result).addClass('img-thumbnail');
         };
 
         reader.readAsDataURL(input.files[i]);
