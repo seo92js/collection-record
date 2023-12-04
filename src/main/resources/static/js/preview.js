1573261f7f59da450a2688d77f366f2f59db6519
seo92js <seo92js@gmail.com>
2023-12-04 18:37:51 +0900

    bootstrap 적용 중

--- src/main/resources/static/js/preview.js
+++ src/main/resources/static/js/preview.js
@@ -1,29 +1,32 @@
-//function preview(input){
-//  $('#preview').empty();
-//
-//  if (input.files) {
-//    for (var i = 0; i < input.files.length; i++){
-//        var reader = new FileReader();
-//        reader.onload = function(e) {
-//            $('#preview').attr("src", e.target.result).addClass('posts-add__preview');
-//        };
-//
-//        reader.readAsDataURL(input.files[i]);
-//    }
-//  } else {
-//    $('#preview').empty();
-//  }
-//}
-
-function preview(input){
+function previewProfile(input){
   $('#preview').empty();
 
   if (input.files) {
     for (var i = 0; i < input.files.length; i++){
         var reader = new FileReader();
         reader.onload = function(e) {
+            $('#preview').attr("src", e.target.result).addClass('image-profile');
+        };
+
+        reader.readAsDataURL(input.files[i]);
+    }
+  } else {
+    $('#preview').empty();
+  }
+}
+
+function previewUpload(input){
+  $('#preview').empty();
+
+  if (input.files) {
+    for (var i = 0; i < input.files.length; i++){
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var div = $('<div>').addClass('image-upload')
             var imgElement = $('<img>').attr('src', e.target.result).addClass('img-thumbnail');
-            $('#preview').append(imgElement);
+            div.append(imgElement);
+            //$('#preview').append(imgElement);
+            $('#preview').append(div);
         };
 
         reader.readAsDataURL(input.files[i]);
